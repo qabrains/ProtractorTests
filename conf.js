8e331b87f6f1320de1c42c74ea4105e3930f1a0d
Kapil Sethi <kapil.sethi9@gmail.com>
2015-12-03 11:29:42 +1100

    Enabling specific test suite execution

--- conf.js
+++ conf.js
@@ -1,9 +1,15 @@
 exports.config = {
 	framework: 'jasmine2',
-	seleniumAddress: 'http://localhost:4444/wd/hub',
+	//seleniumAddress: 'http://localhost:4444/wd/hub',
 	specs: [
 	'tests/*-spec.js'],
-	
+
+	//for running specific test suites
+	suites:{
+		smoke:'tests/smoke_tests/*.js',
+		full:'tests/*.js'
+	},
+
 	capabilities:{
 		//browserName: 'phantomjs', 'phantomjs.binary.path': require('phantomjs').path,
 		browserName: 'chrome',
