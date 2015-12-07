6db67c60f563d2e21bce9f7ebd74c21626752f5b
Kapil Sethi <kapil.sethi9@gmail.com>
2015-12-08 10:57:51 +1100

    Generating test execution report

--- conf.js
+++ conf.js
@@ -1,3 +1,5 @@
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+
 exports.config = {
 	framework: 'jasmine2',
 	//seleniumAddress: 'http://localhost:4444/wd/hub',
@@ -16,15 +18,21 @@ exports.config = {
 		shardTestFiles: true,
 		maxInstances: 2},
 
-	multiCapabilities: [
-		{browserName: 'firefox',
-		shardTestFiles: true,
-		maxInstances: 2},
-		{browserName: 'chrome',
-		shardTestFiles: true,
-		maxInstances: 3},
-		{browserName: 'phantomjs', 'phantomjs.binary.path': require('phantomjs').path,
-		shardTestFiles: true,
-		maxInstances: 3},
-	],
+	//multiCapabilities: [
+	//	{browserName: 'firefox',
+	//	shardTestFiles: true,
+	//	maxInstances: 2},
+	//	{browserName: 'chrome',
+	//	shardTestFiles: true,
+	//	maxInstances: 3},
+	//	{browserName: 'phantomjs', 'phantomjs.binary.path': require('phantomjs').path,
+	//	shardTestFiles: true,
+	//	maxInstances: 3},
+	//],
+	onPrepare: function() {
+		jasmine.getEnv().addReporter(
+			new Jasmine2HtmlReporter({
+			savePath: 'jasmine2-html-reporter-results/screenshots'})
+		);
+	}
 };
