13bfd70f9f6cfa365c3de768135567deb270eeab
Kapil Sethi <Kapil.Sethi@Nintex.com>
2017-08-31 16:22:28 +1000

    updating conf file to have an option to run tests using chrome headless browser

--- local.conf.js
+++ local.conf.js
@@ -2,33 +2,50 @@ var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 
 exports.config = {
 	framework: 'jasmine2',
-	//seleniumAddress: 'http://localhost:4444/wd/hub',
+	// seleniumAddress: 'http://localhost:4444/wd/hub',
 	specs: [
 	'tests/*-spec.js'],
 
-	//for running specific test suites
+	// to run specific test suites
 	suites:{
 		smoketest:'tests/demo-with-*.js',
 		regression:'tests/*-spec.js'
 	},
 
 	capabilities:{
-		// browserName: 'phantomjs', 'phantomjs.binary.path': require('phantomjs').path,
 		browserName: 'chrome',
 		shardTestFiles: true,
-		maxInstances: 3},
+		maxInstances: 3,
+		
+		// uncomment below chromeOptions section to run tests using chrome headless browser
+		// chromeOptions: {
+ 		// 	 args: [ "--headless", "--disable-gpu", "--window-size=800x600" ]
+		// }	
+	},
 
-	//multiCapabilities: [
-	//	{browserName: 'firefox',
-	//	shardTestFiles: true,
-	//	maxInstances: 2},
-	//	{browserName: 'chrome',
-	//	shardTestFiles: true,
-	//	maxInstances: 3},
-	//	{browserName: 'phantomjs', 'phantomjs.binary.path': require('phantomjs').path,
-	//	shardTestFiles: true,
-	//	maxInstances: 3},
-	//],
+	// uncomment below capabilities section to run tests using FF browser
+	// #########################################################################
+	// capabilities:{
+	// 	browserName: 'firefox',
+	// 	shardTestFiles: true,
+	// 	maxInstances: 3
+	// },
+	// #########################################################################
+
+	// #########################################################################
+	// uncomment below multiCapabilities section to run same set of tests using FF and chrome browsers 
+	// multiCapabilities: [
+		
+	// 	{browserName: 'firefox',
+	// 	shardTestFiles: true,
+	// 	maxInstances: 3},
+
+	// 	{browserName: 'chrome',
+	// 	shardTestFiles: true,
+	// 	maxInstances: 3},
+	// ],
+	// #########################################################################
+	
 	onPrepare: () => {
 		jasmine.getEnv().addReporter(
 			new Jasmine2HtmlReporter({
